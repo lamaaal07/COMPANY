3387985cf16498156c84a5a968f565b3d72789aa
Renad <renad0523@gmail.com>
2026-04-16 23:05:49 +0300

    Add files via upload

---
+++ COMPANY.sql
@@ -0,0 +1,27 @@
+use company;
+
+
+
+create table DEPARTMENT (
+    Dname VARCHAR(15) NOT NULL,
+    Dnumber INT NOT NULL,
+    Mgr_ssn CHAR(9) NOT NULL,
+    Mgr_start_date DATE,
+    PRIMARY KEY (Dnumber),
+    UNIQUE (Dname)
+);
+
+CREATE TABLE EMPLOYEE (
+    Fname VARCHAR(15) NOT NULL,
+    Minit CHAR,
+    Lname VARCHAR(15) NOT NULL,
+    Ssn CHAR(9) NOT NULL,
+    Bdate DATE,
+    Address VARCHAR(30),
+    Sex CHAR,
+    Salary DECIMAL(10,2),
+    Super_ssn CHAR(9),
+    Dno INT NOT NULL,
+    PRIMARY KEY (Ssn)
+);
+
